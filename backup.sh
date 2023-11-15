159200988cc4c62505bd2d8004359896479657c2
Aitor <127235588+aitorGonzalo@users.noreply.github.com>
2023-11-15 17:12:46 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir /var/tmp/Backups/$(date +%d-%m-%Y)
-rsync -av --link-dest=/var/tmp/Backups/$(date -d yesterday +%d-%m-%Y) $HOME/Seguridad /var/tmp/Backups/$(date +%d-%m-%Y)
+rsync -av --link-dest=/var/tmp/Backups/$(date -d yesterday +%d-%m-%Y) $HOME/Seguridad/ /var/tmp/Backups/$(date +%d-%m-%Y)
